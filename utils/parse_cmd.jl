16277653103fc70aae892ed22444bfc886da2c6a
Ziyi (Francis) Yin <ziyi.yin@gatech.edu>
2021-09-10 23:47:09 -0400

    add parsing util

---
+++ utils/parse_cmd.jl
@@ -0,0 +1,26 @@
+function parse_commandline()
+    s = ArgParseSettings()
+    @add_arg_table s begin
+        "--nv"
+            help = "Number of vintages"
+            arg_type = Int
+            default = 2
+        "--nsrc"
+            help = "Number of sources per vintage"
+            arg_type = Int
+            default = 2
+        "--vm"
+            help = "Type of virtual machine"
+            arg_type = String
+            default = "Standard_F4"
+        "--nth"
+            help = "Number of threads on a single node"
+            arg_type = Int
+            default = 4
+        "--niter"
+            help = "JRM iterations"
+            arg_type = Int
+            default = 16
+    end
+    return parse_args(s)
+end
