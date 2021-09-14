aea1110406fb414fcee9ffb4f470651b7f56817a
Ziyi (Francis) Yin <ziyi.yin@gatech.edu>
2021-09-14 01:31:04 -0400

    larger instance

--- runall.sh
+++ runall.sh
@@ -14,12 +14,12 @@ wget https://www.dropbox.com/s/radan2hxgb5jnc5/Conc.jld2
 cd ../scripts
 
 L=${L:=2}
-nsrc=${nsrc:=8}
-vm=${vm:=Standard_F4}
-nth=${nth:=4}
+nsrc=${nsrc:=4}
+vm=${vm:=Standard_F8}
+nth=${nth:=8}
 niter=${niter:=16}
 bs=${bs:=4}
-snr=${bs:=0}
+snr=${snr:=0}
 
 julia ConcToV.jl --nv $L
 julia GenLinearData.jl --nv $L --nsrc $nsrc --vm $vm --nth $nth
