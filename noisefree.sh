20ae119cb74e9e64bb491571f1f4be11fb1f285f
Ziyi (Francis) Yin <ziyi.yin@gatech.edu>
2021-09-26 23:14:41 -0600

    smaller step

--- noisefree.sh
+++ noisefree.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-mkdir results
-
-mkdir models
-cd models
-wget https://www.dropbox.com/s/n2cc5nntameh4rg/Compass_tti_625m.jld2
-cd ..
-
-mkdir data
-cd data
-wget https://www.dropbox.com/s/radan2hxgb5jnc5/Conc.jld2
-
-cd ../scripts
+cd scripts
 
 L=${L:=2}
 nsrc=${nsrc:=4}
@@ -22,6 +11,5 @@ bs=${bs:=4}
 snr=${snr:=0}
 gamma=${gamma:=1}
 
-julia ConcToV.jl --nv $L
-julia GenLinearData.jl --nv $L --nsrc $nsrc --vm $vm --nth $nth
-julia JRMsim.jl --nv $L --nsrc $nsrc --vm $vm --nth $nth --niter $niter --bs $bs --gamma $gamma
+julia -p 2 -L ~/startup.jl GenNonlinearData.jl --nv $L --nsrc $nsrc --vm $vm --nth $nth
+julia -p 2 -L ~/startup.jl JRMsim1bg.jl --nv $L --nsrc $nsrc --vm $vm --nth $nth --niter $niter --bs $bs --gamma $gamma
