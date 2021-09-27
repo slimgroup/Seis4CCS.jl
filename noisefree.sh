f5df6bfd257578aeeb38b260ca711ba35df8a427
Ziyi (Francis) Yin <ziyi.yin@gatech.edu>
2021-09-26 23:16:26 -0600

    update

--- noisefree.sh
+++ noisefree.sh
@@ -2,14 +2,14 @@
 
 cd scripts
 
-L=${L:=2}
-nsrc=${nsrc:=4}
+L=${L:=64}
+nsrc=${nsrc:=16}
 vm=${vm:=Standard_E8s_v3}
 nth=${nth:=8}
-niter=${niter:=16}
-bs=${bs:=4}
+niter=${niter:=6}
+bs=${bs:=8}
 snr=${snr:=0}
-gamma=${gamma:=1}
+gamma=${gamma:=16}
 
 julia -p 2 -L ~/startup.jl GenNonlinearData.jl --nv $L --nsrc $nsrc --vm $vm --nth $nth
 julia -p 2 -L ~/startup.jl JRMsim1bg.jl --nv $L --nsrc $nsrc --vm $vm --nth $nth --niter $niter --bs $bs --gamma $gamma
