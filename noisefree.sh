fc137b289f6f71d4360d0b5117c61fa812a38b81
Ziyi (Francis) Yin <ziyi.yin@gatech.edu>
2021-09-28 21:10:49 -0600

    try 32 src for each vintage

--- noisefree.sh
+++ noisefree.sh
@@ -2,14 +2,14 @@
 
 cd scripts
 
-L=${L:=64}
-nsrc=${nsrc:=16}
+L=${L:=2}
+nsrc=${nsrc:=32}
 vm=${vm:=Standard_E8s_v3}
 nth=${nth:=8}
-niter=${niter:=6}
+niter=${niter:=8}
 bs=${bs:=8}
 snr=${snr:=0}
-gamma=${gamma:=16}
+gamma=${gamma:=0.5}
 
 julia -p 2 -L ~/startup.jl GenNonlinearData.jl --nv $L --nsrc $nsrc --vm $vm --nth $nth
 julia -p 2 -L ~/startup.jl JRMsim1bg.jl --nv $L --nsrc $nsrc --vm $vm --nth $nth --niter $niter --bs $bs --gamma $gamma
