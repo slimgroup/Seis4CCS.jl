ce3f868ee43ccc7c24be3611828842bd0db6719b
Ziyi (Francis) Yin <ziyi.yin@gatech.edu>
2022-06-08 10:24:58 -0400

    typo

--- src/Seis4CCS.jl
+++ src/Seis4CCS.jl
@@ -11,11 +11,8 @@ using JOLI, JUDI, FwiFlow
 export Seis4CCSPATH
 Seis4CCSPATH = dirname(pathof(Seis4CCS))
 
-# submodule Simulation
-include("Simulation/Simulation.jl")
-
-# submodule Imaging
-include("Imaging/Imaging.jl")
+include("FlowSimulation/FlowSimulation.jl")
+include("RockPhysics/RockPhysics.jl")
 
 
 end
