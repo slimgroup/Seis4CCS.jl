451f17d60d995be99d75a5956db7733c73567c65
Ziyi (Francis) Yin <ziyi.yin@gatech.edu>
2022-06-16 21:34:28 -0400

    update patchy to work with Compass/channel problem

--- src/RockPhysics/RockPhysicsFunctions.jl
+++ src/RockPhysics/RockPhysicsFunctions.jl
@@ -2,8 +2,39 @@ export Patchy, find_water_bottom
 
 #### Patchy saturation model adapted from FwiFlow.jl
 
+function Patchy(sw::AbstractMatrix{T1}, vp::AbstractMatrix{T}, rho::AbstractMatrix{T}, phi::AbstractMatrix{T}; bulk_min = 36.6f9, bulk_fl1 = 2.735f9, bulk_fl2 = 0.125f9, ρw = 501.9f0, ρo = 1053.0f0) where {T1, T}
+
+    ### works for channel problem
+    vs = vp./sqrt(3f0)
+    sw = T.(sw)
+    bulk_sat1 = rho .* (vp.^2f0 - 4f0/3f0 .* vs.^2f0)
+    shear_sat1 = rho .* (vs.^2f0)
+
+    patch_temp = bulk_sat1 ./(bulk_min .- bulk_sat1) - 
+    bulk_fl1 ./ phi ./ (bulk_min .- bulk_fl1) + 
+    bulk_fl2 ./ phi ./ (bulk_min .- bulk_fl2)
+
+    bulk_sat2 = bulk_min./(1f0./patch_temp .+ 1f0)
+
+    bulk_new = 1f0./( (1f0.-sw)./(bulk_sat1+4f0/3f0*shear_sat1) 
+    + sw./(bulk_sat2+4f0/3f0*shear_sat1) ) - 4f0/3f0*shear_sat1
+
+    rho_new = rho + phi .* sw * (ρw - ρo)
+
+    Vp_new = sqrt.((bulk_new+4f0/3f0*shear_sat1)./rho_new)
+    return Vp_new, rho_new
+
+end
+
+function Patchy(sw::AbstractArray{T1, 3}, vp::AbstractMatrix{T}, rho::AbstractMatrix{T}, phi::AbstractMatrix{T}; bulk_min = 36.6f9, bulk_fl1 = 2.735f9, bulk_fl2 = 0.125f9, ρw = 501.9f0, ρo = 1053.0f0) where {T1, T}
+
+    stack = [Patchy(sw[i,:,:], vp, rho, phi; bulk_min=36.6f9, bulk_fl1=bulk_fl1, bulk_fl2=bulk_fl2, ρw = ρw, ρo=ρo) for i = 1:size(sw,1)]
+    return [stack[i][1] for i = 1:size(sw,1)], [stack[i][2] for i = 1:size(sw,1)]
+end
+
 function Patchy(sw::AbstractMatrix{T1}, vp::AbstractMatrix{T}, rho::AbstractMatrix{T}, phi::AbstractMatrix{T}, d::Tuple{T, T}; bulk_fl1 = 2.735f9, bulk_fl2 = 0.125f9,ρw = 7.766f2, ρo = 1.053f3) where {T1, T}
 
+    ### works for Compass 2D model
     n = size(sw)
     capgrid = Int(round(50f0/d[2]))
     vp = vp * 1f3
@@ -42,7 +73,7 @@ function Patchy(sw::AbstractMatrix{T1}, vp::AbstractMatrix{T}, rho::AbstractMatr
 end
 function Patchy(sw::AbstractArray{T1, 3}, vp::AbstractMatrix{T}, rho::AbstractMatrix{T}, phi::AbstractMatrix{T}, d::Tuple{T, T}; bulk_fl1 = 2.735f9, bulk_fl2 = 0.125f9,ρw = 7.766f2, ρo = 1.053f3) where {T1, T}
 
-    stack = [Patchy(T.(sw[i,:,:]), vp, rho, phi, d; bulk_fl1=bulk_fl1, bulk_fl2=bulk_fl2, ρw = ρw, ρo=ρo) for i = 1:size(sw,1)]
+    stack = [Patchy(sw[i,:,:], vp, rho, phi, d; bulk_fl1=bulk_fl1, bulk_fl2=bulk_fl2, ρw = ρw, ρo=ρo) for i = 1:size(sw,1)]
     return [stack[i][1] for i = 1:size(sw,1)], [stack[i][2] for i = 1:size(sw,1)]
 end
 
